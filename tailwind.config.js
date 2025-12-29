e85f691072553817e203a0d1aa7b34d38eb76fcd
Austin-ops97 <aus.l.alexander@gmail.com>
2025-12-29 13:18:08 -0600

    Enable Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./App.tsx",
+    "./index.tsx",
+    "./**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
